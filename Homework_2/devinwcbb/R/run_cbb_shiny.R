5e1df56d38c02b56415183e96f46f286a8849666
Devin Warner <devinwarner10@gmail.com>
2022-02-16 19:41:08 -0700

    Second commit. Finished question 2. Updated both packages to run their shiny apps with a run_shiny function.

---
+++ Homework_2/devinwcbb/R/run_cbb_shiny.R
@@ -0,0 +1,15 @@
+#' Runs the shiny app.
+#'
+#' I mean, thats about as much explanation as it needs
+#'
+#' @export
+#'
+
+run_cbb_shiny <- function() {
+  appDir <- system.file("cbb_shiny.R", package = "devinwcbb")
+  if (appDir == "") {
+    stop("Could not find myapp. Try re-installing `devinwcbb`.", call. = FALSE)
+  }
+  
+  shiny::runApp(appDir, display.mode = "normal")
+}
